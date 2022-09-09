707182edf5b26f782a409e836e73305beaf4383f
unknown <jrodriguezr28@miumg.edu.gt>
2022-09-08 23:18:33 -0600

    Agregar Modelo Persona

---
+++ App_WebMVC/Models/Persona.cs
@@ -0,0 +1,11 @@
+﻿namespace App_WebMVC.Models
+{
+    public class Persona
+    {
+
+        public int idPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
